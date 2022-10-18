8916d90085f70e1983ddc815d2014f45fbb2fa7e
zhiwei.wang <zhiwei.wang@horizon.ai>
2022-10-18 22:27:08 +0800

    增加姿势识别传输

---
+++ central_node/build/CMakeFiles/central_node.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/4-WorkFile/1-MyDemoTest/Hackton/Hackton/HackProject/central_node/gesture.cpp" "CMakeFiles/central_node.dir/gesture.cpp.obj" "gcc" "CMakeFiles/central_node.dir/gesture.cpp.obj.d"
+  "D:/4-WorkFile/1-MyDemoTest/Hackton/Hackton/HackProject/central_node/imu.cpp" "CMakeFiles/central_node.dir/imu.cpp.obj" "gcc" "CMakeFiles/central_node.dir/imu.cpp.obj.d"
+  "D:/4-WorkFile/1-MyDemoTest/Hackton/Hackton/HackProject/central_node/main.cpp" "CMakeFiles/central_node.dir/main.cpp.obj" "gcc" "CMakeFiles/central_node.dir/main.cpp.obj.d"
+  "D:/4-WorkFile/1-MyDemoTest/Hackton/Hackton/HackProject/central_node/posture.cpp" "CMakeFiles/central_node.dir/posture.cpp.obj" "gcc" "CMakeFiles/central_node.dir/posture.cpp.obj.d"
+  "D:/4-WorkFile/1-MyDemoTest/Hackton/Hackton/HackProject/central_node/snowman.cpp" "CMakeFiles/central_node.dir/snowman.cpp.obj" "gcc" "CMakeFiles/central_node.dir/snowman.cpp.obj.d"
+  "D:/4-WorkFile/1-MyDemoTest/Hackton/Hackton/HackProject/central_node/socket_link.cpp" "CMakeFiles/central_node.dir/socket_link.cpp.obj" "gcc" "CMakeFiles/central_node.dir/socket_link.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ central_node/build/CMakeFiles/central_node.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for central_node.
+# This may be replaced when dependencies are built.
